3bc56641ed441e1204423bd9bd2bc326b30e6edb
Sopss <molinacarlos94@gmail.com>
2018-10-17 07:58:01 +0200

    Fin de avances en clase

--- gestionPersonas/Persona.vb
+++ gestionPersonas/Persona.vb
@@ -2,17 +2,50 @@
 	Private nombre As String
 	Private dni As String
 	Private sexo As String
+	Private edad As Integer
 	Private peso As Double
 	Private altura As Double
 
 
-	Sub New(ByVal nombre As String, ByVal dni As String, ByVal sexo As String)
-
+	Sub New(ByVal nombre As String, ByVal edad As Integer, ByVal sexo As String)
+		Me.nombre = nombre
+		Me.edad = edad
+		Me.sexo = sexo
 	End Sub
 	Sub New()
 
 	End Sub
 
+	Sub New(ByVal dni As String, ByVal nombre As String, ByVal edad As Integer, ByVal sexo As String, ByVal peso As Double, ByVal altura As Double)
+		Me.nombre = nombre
+		Me.edad = edad
+		Me.sexo = sexo
+		Me.altura = altura
+		Me.peso = peso
+		Me.dni = dni
+	End Sub
+
+
+	Public Function CalcularIMC() As Integer
+		If (Me.peso / (altura * altura) >= 30) Then 'Estassss GOOOORDO
+			Return 2
+		ElseIf (Me.peso / (altura * altura) >= 25 And Me.peso / (altura * altura) < 30) Then 'Te sobren els torrons de nadal
+			Return 1
+		ElseIf (Me.peso / (altura * altura) >= 18.5 And Me.peso / (altura * altura) < 25) Then ' Estas de puta mare
+			Return 0
+		ElseIf (Me.peso / (altura * altura) < 18.5) Then 'Tio, eres tan pleb, que no tens ni pa menjar
+			Return -1
+		End If
+	End Function
+
+	Public Function esMayorDeEdad() As Boolean
+		If (Me.edad >= 18) Then
+			Return True
+		Else
+			Return False
+		End If
+
+	End Function
 
 
 
